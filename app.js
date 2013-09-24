e8314a3c0c851dfeadea54b8c32b185933c44c4e
Jason More <Jason.More@gmail.com>
2013-09-24 09:24:39 -0500

    updated server bundling

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var mongoUrl = process.env.MONGO_URL || process.env.MONGOHQ_URL ||
   'mongodb://localhost:27017/project';
 
 var store = racer.createStore({
-	server: server,
+  server: server,
   db: liveDbMongo(mongoUrl + '?auto_reconnect', {safe: true}),
   redis: redis
 });
@@ -32,31 +32,28 @@ app.use(require('racer-browserchannel')(store));
 app.use(express.bodyParser());
 
 app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/public/index.htm');
+  res.sendfile(__dirname + '/public/index.htm');
 });
 
 app.get('/model', function (req, res) {
-	var model = store.createModel();
-	model.subscribe('entries', function (err, entries) {
-		if (err) {
-			res.status(500);
-			res.send(err);
-		} else {
-			model.bundle(function (err, bundle) {
-				res.send(JSON.stringify(bundle));
-			});
-		}
-	});
+  var model = store.createModel();
+  model.bundle(function (err, bundle) {
+    if (err) {
+      res.status(500);
+      res.send(err);
+    } else {
+      res.send(JSON.stringify(bundle));
+    }
+  });
 });
 
 
 store.bundle(__dirname + '/client.js', function (err, js) {
-	app.get('/script.js', function (req, res) {
-		res.type('js');
-		res.send(js);
-	});
+  app.get('/script.js', function (req, res) {
+    res.type('js');
+    res.send(js);
+  });
 });
-
 
 
 server.listen(process.env.PORT || 3001);
